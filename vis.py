a59039bb59f0dfe10b14e82c3b27cfc8ac26c8fb
dev <laomaotf@outlook.com>
2025-12-07 13:08:23 +0800

    vis:update

--- vis.py
+++ vis.py
@@ -3,12 +3,13 @@ import geopandas as gpd
 
 longit, lati = 120.1417, 30.2458  
 region_size = 0.05
-# 创建底图
 m = folium.Map(location=[lati, longit], zoom_start=10)
 
-# 添加OSM数据
+m.add_child(folium.ClickForMarker())
+folium.Marker([lati, longit], popup='<i>West lake</i>', icon=folium.Icon(icon='info-sign', color="red")).add_to(m)
+
 gdf = gpd.read_file('./data/planet_119.867_29.995_9f24ee25.osm',
-                    bbox=(longit - region_size/2, lati - region_size/2, longit + region_size/2, lati+region_size/2), layer="points")
+                   bbox=(longit - region_size/2, lati - region_size/2, longit + region_size/2, lati+region_size/2), layer="points")
 folium.GeoJson(gdf).add_to(m)
 m.save('map.html')
 
